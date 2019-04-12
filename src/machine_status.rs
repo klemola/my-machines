db4cc6184cdbe72dac4b88c28cba81b3100e775b
Matias Klemola <matias.klemola@gmail.com>
2019-04-12 12:09:22 +0300

    Use updated attribute names and send several updates per machine

--- src/machine_status.rs
+++ src/machine_status.rs
@@ -3,15 +3,19 @@ use dynomite::{
     FromAttributes, Item,
 };
 use hostname::get_hostname;
+use itertools::Itertools;
 use rusoto_core::Region;
 use std::error::Error;
+use time;
+use uuid::Uuid;
 
 #[derive(Item, Debug, Clone)]
 pub struct MachineStatus {
     #[hash]
-    MachineID: String,
-    StatusID: u16,
-    StatusMeta: String,
+    status_id: String,
+    machine_id: String,
+    timestamp: String,
+    status_meta: String,
 }
 
 pub fn get_client() -> DynamoDbClient {
@@ -34,6 +38,7 @@ pub fn list_status(
         .iter()
         .map(|result| MachineStatus::from_attrs(result.clone()))
         .filter_map(|mapped| mapped.ok())
+        .unique_by(|status| status.machine_id.clone())
         .collect::<Vec<MachineStatus>>();
 
     return Ok(status_vec);
@@ -46,9 +51,10 @@ pub fn save_status(
     let hostname = get_hostname().unwrap_or_default();
 
     let machine_status = MachineStatus {
-        MachineID: hostname.into(),
-        StatusID: 0,
-        StatusMeta: String::from("Something"),
+        status_id: Uuid::new_v4().to_string(),
+        machine_id: hostname.into(),
+        timestamp: time::now_utc().rfc3339().to_string(),
+        status_meta: String::from("Something"),
     };
 
     let put_item_input = PutItemInput {
